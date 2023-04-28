ec1b83b440ff78d1344410c4ea9c7ebf129ae745
Diego Monte <diegosilva527@gmail.com>
2023-04-28 00:30:15 -0300

    ajustes na pagina de login, imagem e css

--- src/pages/Home/Index.tsx
+++ src/pages/Home/Index.tsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 import ButtonGoogle from '../../components/ButtonGoogle';
 
-import Image from "react";
+
 export const children = 'Login'
 
 
@@ -16,19 +16,20 @@ export default function Home() {
             <div className="flex items-center">
               <h1>Login</h1>
             </div>
-            <div className="flex flex-col sm:justify-center items-center">
+            <div className="flex flex-col sm:justify-center items-center  padding-form">
               <h2>Faça o Login para acessar</h2>
               <ButtonGoogle onClick={()=>{}}></ButtonGoogle>
               <Input id="email" type="email" className="input" placeholder="E-mail" required={true} />
               <Input id="password" type="password" className="input" placeholder="Senha" required={true} />
+               <Button children={children}></Button>
             </div>
             <div className="flex flex-col sm:justify-center items-center ">
-            <Button children={children}></Button>
+           
             </div>
           </div>
           <div className="bg-gradient-to-br from-purple-800 via-purple-700 to-purple-900 sm:justify-center items-center sm:w-1/2 m:w-full">
-            <div className="w-full sm:max-w-sm-screen h-screen flex flex-col items-center justify-center text-gray-600">
-            xx
+            <div className="w-full sm:max-w-sm-screen h-screen flex flex-col items-center justify-center text-gray-600 login-background ">
+              
             </div>
           </div>
         </div>
