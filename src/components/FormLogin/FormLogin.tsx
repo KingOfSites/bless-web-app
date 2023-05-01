45108d17ad918f3fe55fc2852ff99f0c22171b18
Diego Monte <diegosilva527@gmail.com>
2023-05-01 20:54:05 -0300

    logo

--- src/components/FormLogin/FormLogin.tsx
+++ src/components/FormLogin/FormLogin.tsx
@@ -6,16 +6,21 @@ import ButtonGoogle from '../ButtonGoogle'
 import { Button } from '../ButtonLogom/Index'
 import InputPassword from '../InputPassword/Index'
 import TextOverline from '../TextoOverline/Index'
+import Logo from '../../assets/logo.svg'
 export const children = 'ENTRAR'
+
 
 
 export default function FormLogin() {
   return (
     <>
        <div className="container-form-login ">
-        <div className="column">
-          <h2>Bem-vindo à Bless</h2>
+        <div className="column flex align-items-center  justify-content-center margin-top-20px ">
+          <img src={Logo}  alt="logos"/>
         </div>  
+        <div className="column ">
+          <h2>Bem-vindo à Bless</h2>
+        </div> 
         <div className="column">
           <label className='label'>E-mail</label>
           <input type="text" className='input  input-element'  />
