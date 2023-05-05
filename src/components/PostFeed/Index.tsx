422ed262d416bdc679df49db3c99558dd5ccd130
Guilherme Costa <guilhermelimacosta@gmail.com>
2023-05-05 17:44:11 -0300

    ajuste mobile feed

--- src/components/PostFeed/Index.tsx
+++ src/components/PostFeed/Index.tsx
@@ -11,10 +11,10 @@ function Post() {
     <div className="container-post">
       <div className="main-post">
         <div className="publi-post">
-          <div>
+          <div className="h1">
             <h1>Feed</h1>
           </div>
-          <div>
+          <div className="reload">
             <img src={Reload} alt="Reload" />
           </div>
         </div>
@@ -26,9 +26,40 @@ function Post() {
         </div>
       </div>
       <div className="container-button">
-        <button className="button-feed">Fenimino</button>
-        <button className="">Masculino </button>
-        <button className="button-feed">LGBTAIA+</button>
+        <div className="btn-div">
+          <button className="button-feed">Fenimino</button>
+          <button className="">Masculino </button>
+          <button className="button-feed">LGBTAIA+</button>
+        </div>
+      </div>
+      <div className="container-postagem">
+        <div className="info-postagem">
+          <div className="criador">
+            <button></button>
+            <h3>
+              Criador nome <br />
+              <span className="span-h3">@criador</span>
+            </h3>
+          </div>
+          <div className="data-postagem">
+            <p>14 de abril</p>
+          </div>
+        </div>
+        <div className="text-publi">
+          <p>
+            A Sobriety Story | Why I Quit Alcohol | Julian ShawIf you've been
+            thinking about sobriety, watch @julianshaw's video on his journey to
+            sobriety and everything he has gained since leaving alcohol behind!
+            ...
+          </p>
+        </div>
+        <div className="foto-postagem">
+          <img src={PostImagem} alt="PostImagem" />
+        </div>
+        <div className="curtida-cometario">
+          <img src={Curtida} alt="Curtida" />{" "}
+          <img src={Comentario} alt="Comentario" />{" "}
+        </div>
       </div>
       <div className="container-postagem">
         <div className="info-postagem">
